53aa469a7d4222c70735ab53492e489959c68207
Anup Mankar <amankar@sapient.com>
2019-06-09 15:19:03 -0400

    Added spinner native module

--- packages/app/App.js
+++ packages/app/App.js
@@ -10,14 +10,18 @@ import React from 'react';
 import {Platform, StyleSheet, Text, View, FlatList, AppRegistry} from 'react-native';
 import NewsHeadlines from '@tcp/core/lib/core';
 
+
+import Spinner from "react-native-spinkit";
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
 });
 
 type Props = {}; 
+
 
 export default class AjaxFlatList extends React.Component {
   constructor(props) {
@@ -34,9 +38,10 @@ export default class AjaxFlatList extends React.Component {
     const response = await newsApi.getHeadlines();
     if (response.data && response.data.articles) {
       let headlines = response.data.articles;
-      this.setState({
-        headlines: headlines
-      })
+      setTimeout(()=> {this.setState({
+          headlines: headlines
+        })
+      }, 2000);
     }
   }
   render = () => {
@@ -51,6 +56,7 @@ export default class AjaxFlatList extends React.Component {
       :
       (<View style={styles.container}>
         <Text style={styles.welcome}>Headlines of the hour</Text>
+        <Spinner type="CircleFlip" isVisible="true" style={styles.spinner} color="#333333"/>
       </View>)
 
     )
@@ -75,4 +81,8 @@ const styles = StyleSheet.create({
     color: '#000',
     marginBottom: 5,
   },
+  spinner: {
+    alignSelf: 'center',
+    marginBottom: 50
+  }
 });
